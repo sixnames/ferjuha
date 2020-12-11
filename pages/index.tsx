5085b2bf209057216652c852796967f81b3c3471
sixnames <slava.kirshman@icloud.com>
2020-12-11 19:36:21 +0200

    cluster connected

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,32 @@
 import Head from 'next/head';
+import { getDatabase } from '../db/mongodb';
 
 export default function Home() {
   return (
     <div>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href={"/favicon.ico"} />
+        <link rel='icon' href={'/favicon.ico'} />
       </Head>
 
-      
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid consectetur debitis
+        delectus eaque eum id, impedit ipsam itaque iure natus nihil omnis, reiciendis similique sit
+        voluptates! Amet illo omnis tempora!
+      </div>
     </div>
-  )
+  );
+}
+
+export async function getStaticProps() {
+  const db = await getDatabase();
+  const collection = db.collection('items');
+  const items = await collection.find({}).toArray();
+
+  return {
+    props: {
+      items: JSON.parse(JSON.stringify(items)),
+    },
+    revalidate: 30,
+  };
 }
